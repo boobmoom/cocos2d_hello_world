48f3d04c69215099e7a0a5bec05b9fe40e929321
tom.ma <tom.ma@i-click.cn>
2012-01-13 15:07:11 +0800

    modified:   hello_world.xcodeproj/project.pbxproj 	modified:   hello_world.xcodeproj/xcuserdata/iclick.xcuserdatad/xcdebugger/Breakpoints.xcbkptlist  	deleted:    hello_world/ControlLayer.h  	deleted:    hello_world/ControlLayer.m  	deleted:    hello_world/GameLayer.h  	deleted:    hello_world/GameLayer.m  	modified:   hello_world/XminLayer.h  	modified:   hello_world/XminLayer.m

--- hello_world/XminLayer.h
+++ hello_world/XminLayer.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 #import "Player.h"
-#import "ControlLayer.h"
+
 
 enum {
     kTagForPlayer,
--- hello_world/XminLayer.m
+++ hello_world/XminLayer.m
@@ -20,7 +20,39 @@
 {
     CCScene *scene = [CCScene node];
     XminLayer *layer = [XminLayer node];
-    ControlLayer *control_layer = [ControlLayer nodeReceiver: layer];
+
+    
+    CCSprite *down_button = [CCSprite spriteWithFile:@"down.png"];
+    CCSprite *down_button_selected = [CCSprite spriteWithFile:@"down.png"];
+    CCMenuItemSprite *downItem = [CCMenuItemSprite itemFromNormalSprite: down_button selectedSprite:down_button_selected target: layer selector: @selector(walkDown)];
+    downItem.position = CGPointMake(40 + 20, 20);
+    
+    CCSprite *left_button = [CCSprite spriteWithFile:@"down.png"];
+    CCSprite *left_button_selected = [CCSprite spriteWithFile:@"down.png"];
+    CCMenuItemSprite *leftItem = [CCMenuItemSprite itemFromNormalSprite: left_button selectedSprite:left_button_selected  target: layer selector: @selector(walkLeft)];
+    leftItem.rotation = 90;
+    leftItem.position = CGPointMake(20 , 40+20);
+    
+    CCSprite *right_button = [CCSprite spriteWithFile:@"down.png"];
+    CCSprite *right_button_selected = [CCSprite spriteWithFile:@"down.png"];
+    CCMenuItemSprite *rightItem = [CCMenuItemSprite itemFromNormalSprite: right_button selectedSprite:right_button_selected target: layer selector: @selector(walkRight)];
+    rightItem.rotation = -90;
+    rightItem.position = CGPointMake(80 + 20, 40+20);
+    
+    CCSprite *up_button = [CCSprite spriteWithFile:@"down.png"];
+    CCSprite *up_button_selected = [CCSprite spriteWithFile:@"down.png"];
+    CCMenuItemSprite *upItem = [CCMenuItemSprite itemFromNormalSprite: up_button selectedSprite:up_button_selected target: layer selector: @selector(walkUp)];
+    upItem.rotation = 180;
+    upItem.position = CGPointMake(40+20, 80+20);
+    
+    CCMenu *control_layer = [CCMenu menuWithItems: downItem , rightItem , leftItem , upItem ,nil];
+    
+    control_layer.contentSize = CGSizeMake(120, 120);
+    CGSize win_size = [[CCDirector sharedDirector] winSize];
+    control_layer.position = CGPointMake(win_size.width - 120 ,0);
+    control_layer.scale = 0.6;
+    
+    
     [scene addChild: layer];
     [scene addChild: control_layer];
 
@@ -28,6 +60,26 @@
     return scene;
 }
 
+
+-(void) walkDown
+{
+    [self command: @"down"];
+}
+
+-(void) walkUp
+{
+    [self command: @"up"];    
+}
+
+- (void) walkLeft
+{
+    [self command: @"left"];    
+}
+
+- (void) walkRight
+{
+    [self command: @"right"];    
+}
 
 - (CCSprite *) playerSprite
 {
